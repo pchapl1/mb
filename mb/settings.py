d13aaea4769cd7797ac3b5a2afabc1566d0a5fe4
Phil Chaplin <pchapl1@gmail.com>
2022-04-30 12:51:09 -0600

    added allowed hosts

--- mb/settings.py
+++ mb/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
-ALLOWED_HOSTS = []
+# DEBUG = True
+DEBUG = False
+ALLOWED_HOSTS = ['https://django-m.herokuapp.com/']
 
 
 # Application definition
