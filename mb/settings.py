c0cd8f74070dbc718dfa87b53269076b99c75cf3
Phil Chaplin <pchapl1@gmail.com>
2022-04-30 12:59:50 -0600

    changed allowed hosts

--- mb/settings.py
+++ mb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = False
-ALLOWED_HOSTS = ['https://django-m.herokuapp.com/']
+ALLOWED_HOSTS = ['.herokuapp.com','localhost', '127.0.0.1']
 
 
 # Application definition
